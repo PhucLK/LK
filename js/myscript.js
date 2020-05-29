d000da923a732adf4dc2e7d294460ce33d9b2714
PhucLK <phuclkcarryonto04@gmail.com>
2020-05-29 10:30:05 +0700

    add mouseover image card

--- js/myscript.js
+++ js/myscript.js
@@ -1,34 +1,42 @@
 
-    $(document).ready(() =>{
-        //find if window width less than 777px then remove class
-        let lastWidth = $(window).width();
-        $(window).resize(function(){
-          if($(window).width()!=lastWidth){
+$(document).ready(() => {
+  //find if window width less than 500px then remove class
+  let lastWidth = $(window).width();
+  $(window).resize(function () {
+    if ($(window).width() != lastWidth) {
 
-              lastWidth = $(window).width();
-              
-              if(lastWidth < 777){
-                $(".carousel-infor").hide();
-              }else{
-                $(".carousel-infor").show();
-              }
-          }
-        });
+      lastWidth = $(window).width();
 
-        //if user scroll page to bottom > 240px then show button scroll to top in 800 milisecon
-          $(window).scroll(function(){
-            if ($(this).scrollTop() > 240){
-              $('#myBtn').show();
-            } 
-            else{
-              $('#myBtn').hide();
-            }
-          });
+      if (lastWidth < 500) {
+        $(".carousel-infor").hide();
+      } else {
+        $(".carousel-infor").show();
+      }
+    }
+  });
 
-          $('#myBtn').click(function(){
+  //if user scroll page to bottom > 240px then show button scroll to top in 800 milisecon
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 240) {
+      $('#myBtn').show();
+    }
+    else {
+      $('#myBtn').hide();
+    }
+  });
 
-            $("html, body").animate({scrollTop : 0},800);
-            //return false;
-          });
+  $('#myBtn').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 
-    });
+  $('.circle').on('mouseover', function () {
+    $(this).css("width", "12rem");
+    return false;
+  });
+
+  $('.circle').on('mouseout', function () {
+    $(this).css("width", "10rem");
+    return false;
+  });
+});
